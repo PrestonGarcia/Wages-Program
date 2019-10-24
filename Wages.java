2b976c9e2f9b4b17c4fe8288ff504a27c49c6e95
PrestonGarcia <prestongarcia77@gmail.com>
2019-10-23 14:18:33 -1000

    Took out class file, fixed comment formatting, made it so that scanner resets before each input

--- Wages.java
+++ Wages.java
@@ -34,7 +34,7 @@ public class Wages{//Start bracket for Wages class
       boolean bAllDone[] = {bEmployeeNameDone, bTimeDone, bPayPerHourDone, bMoreEmployeesDone};
       boolean bProgramDone = false;
       
-      //char variable
+      //char variables
       char cLastEmployee;
       char cTensPlaceCheckIn;
       char cTensPlaceCheckOut;
@@ -76,20 +76,26 @@ public class Wages{//Start bracket for Wages class
          while (!bAllDone[EMPLOYEENAMEINDEX]){ //Start bracket for while loop checking to see if a valid employee name has been given 
             
             try{//Start bracket for try statement
-            
-               System.out.printf("Please enter the employee's name, must have more than %d non-blank characters \n", MINNAMECHARAMOUNT); //Min char amount is 3
-               sEmployeeName = scanner.nextLine().trim(); //Reads the employee name inputted, and takes out blank spaces around
-               scanner = new Scanner(System.in); //Resets scanner
-               sEmployeeNameNoSpaces = sEmployeeName.replace(" ", ""); //Removes blank characters
-               iEmployeeNameCharAmount = sEmployeeNameNoSpaces.length(); //Counts non blank characters
+               //Min char amount is 3           
+               System.out.printf("Please enter the employee's name, must have more than %d non-blank characters \n", MINNAMECHARAMOUNT); 
+               //Resets scanner
+               scanner = new Scanner(System.in); 
+               //Reads the employee name inputted, and takes out blank spaces around
+               sEmployeeName = scanner.nextLine().trim(); 
+               //Removes blank characters
+               sEmployeeNameNoSpaces = sEmployeeName.replace(" ", ""); 
+               //Counts non blank characters
+               iEmployeeNameCharAmount = sEmployeeNameNoSpaces.length(); 
                
                if(iEmployeeNameCharAmount < MINNAMECHARAMOUNT){//If statement checking if name char amount is < 3
                   
-                  throw new InputMismatchException();//Goes to catch statement
+                  //Goes to catch statement
+                  throw new InputMismatchException();
                
                }//End bracket for if statement checking char amount of name
                
-               bAllDone[EMPLOYEENAMEINDEX] = true;//Ends while loop
+               //Ends while loop
+               bAllDone[EMPLOYEENAMEINDEX] = true;
             
             } //End bracket for try statement
             
@@ -105,35 +111,47 @@ public class Wages{//Start bracket for Wages class
             
             try{//Start bracket for try statement
                
-               System.out.printf("Please enter the employee's check-in time in military format. Must be between %3d and %4d \n", MINTIME, MAXTIME);//mintime is 800, maxtime is 1900
-               iCheckInTime = scanner.nextInt(); //Reads next int input
-               scanner = new Scanner(System.in); //Resets scanner
-               sCheckInTime = Integer.toString(iCheckInTime);//Converts to string
-               iCheckInTimeLength = sCheckInTime.length(); //Gets length
-               iCheckInTensPlaceIndex = iCheckInTimeLength - TENSPLACEOFFSET; //2nd to last index of string
-               cTensPlaceCheckIn = sCheckInTime.charAt(iCheckInTensPlaceIndex); //gets char at 2nd to last index
-               iCheckInTensPlace = Character.getNumericValue(cTensPlaceCheckIn); //Converts to int for comparison
+               //mintime is 800, maxtime is 1900
+               System.out.printf("Please enter the employee's check-in time in military format. Must be between %3d and %4d \n", MINTIME, MAXTIME);
+               //Resets scanner
+               scanner = new Scanner(System.in); 
+               //Reads next int input
+               iCheckInTime = scanner.nextInt(); 
+               sCheckInTime = Integer.toString(iCheckInTime);
+               iCheckInTimeLength = sCheckInTime.length(); 
+               //2nd to last index of string
+               iCheckInTensPlaceIndex = iCheckInTimeLength - TENSPLACEOFFSET; 
+               cTensPlaceCheckIn = sCheckInTime.charAt(iCheckInTensPlaceIndex); 
+               //Converts to int for comparison
+               iCheckInTensPlace = Character.getNumericValue(cTensPlaceCheckIn); 
                
-               if (iCheckInTensPlace >= MAXTENSPLACE || iCheckInTime < MINTIME || iCheckInTime > MAXTIME){ //If statement checking if check-in time is outside 0-2400 range
                
-                  throw new InputMismatchException();//Goes to the catch statement
+               if (iCheckInTensPlace >= MAXTENSPLACE || iCheckInTime < MINTIME || iCheckInTime > MAXTIME){ //If statement checking if check-in time is outside 800-1900 range
+               
+                  //Goes to the catch statement
+                  throw new InputMismatchException();
                
                } //End bracket for if statement checking check-in time range
                
-               System.out.printf("Please enter the employee's check-out time in military format. Must be between %3d and %4d \n", MINTIME, MAXTIME); //Asks user for check-out time
-               iCheckOutTime = scanner.nextInt(); //Reads next int input
-               scanner = new Scanner(System.in); //Resets scanner
-               sCheckOutTime = Integer.toString(iCheckOutTime);//Converts to string
-               iCheckOutTimeLength = sCheckOutTime.length(); //Gets length
-               iCheckOutTensPlaceIndex = iCheckOutTimeLength - TENSPLACEOFFSET; //2nd to last index of string
-               cTensPlaceCheckOut = sCheckOutTime.charAt(iCheckOutTensPlaceIndex); //gets char at 2nd to last index
-               iCheckOutTensPlace = Character.getNumericValue(cTensPlaceCheckOut); //Converts to int for comparison
+               //Asks user for check-out time
+               System.out.printf("Please enter the employee's check-out time in military format. Must be between %3d and %4d \n", MINTIME, MAXTIME); 
+               //Resets scanner
+               scanner = new Scanner(System.in); 
+               iCheckOutTime = scanner.nextInt(); 
+               sCheckOutTime = Integer.toString(iCheckOutTime);
+               iCheckOutTimeLength = sCheckOutTime.length(); 
+               //2nd to last index of string
+               iCheckOutTensPlaceIndex = iCheckOutTimeLength - TENSPLACEOFFSET; 
+               cTensPlaceCheckOut = sCheckOutTime.charAt(iCheckOutTensPlaceIndex); 
+               //Converts to int for comparison
+               iCheckOutTensPlace = Character.getNumericValue(cTensPlaceCheckOut); 
                
-               if (iCheckOutTime < iCheckInTime || iCheckOutTensPlace >= MAXTENSPLACE || iCheckOutTime < MINTIME || iCheckOutTime > MAXTIME){//If statement checking if check-out time is outside 0-2400 range
+               if (iCheckOutTime < iCheckInTime || iCheckOutTensPlace >= MAXTENSPLACE || iCheckOutTime < MINTIME || iCheckOutTime > MAXTIME){//If statement checking if check-out time is outside 800-1900 range and greater than checkin
                
-                  throw new InputMismatchException(); //Goes to catch statement
+                  //Goes to catch statement
+                  throw new InputMismatchException(); 
                
-               }//End bracket for if statement checking if check-out time is outside 0-2400 range
+               }//End bracket for if statement checking if check-out time is outside 800-1900 range and greater than checkin
                
                bAllDone[TIMEINDEX] = true; //Ends while loop
             
@@ -157,16 +175,20 @@ public class Wages{//Start bracket for Wages class
             
             try{ //Start bracket for try statement
             
-               System.out.printf("Please enter the employee's pay. Must be between %.2f and %.2f \n", MINPAY, MAXPAY);//Min pay 8.50, max pay 49.99 
-               fEmployeePay = scanner.nextFloat();//Reads next float input
-               scanner = new Scanner(System.in); //Resets scanner
+               //Min pay 8.50, max pay 49.99
+               System.out.printf("Please enter the employee's pay. Must be between %.2f and %.2f \n", MINPAY, MAXPAY); 
+               //Resets scanner
+               scanner = new Scanner(System.in); 
+               fEmployeePay = scanner.nextFloat();
             
                if (fEmployeePay < MINPAY || fEmployeePay > MAXPAY){//If statement checking if input pay is in 8.50 to 49.99 range
             
+                  //Goes to catch statement
                   throw new InputMismatchException();
             
                }//end of if statement checking if input pay is in 8.50 to 49.99 range
                
+               //Ends while loop
                bAllDone[PAYINDEX] = true;
             
             }//End bracket for try statement
@@ -181,26 +203,33 @@ public class Wages{//Start bracket for Wages class
       
          switch (iCheckInTimeLength) {//Start bracket for switch checking length of checkin, to avoid outofbounds index errors
             
-            case FOURLETTERSLONG: iCheckInHours = Integer.parseInt(sCheckInTime.substring(STARTINDEX, ENDINDEXFOURLETTERS)); //turns first two characters of string to int
+            //turns first two characters of string to int, that will be the hour check in is at (ex: 1500 is hour 15) 
+            case FOURLETTERSLONG: iCheckInHours = Integer.parseInt(sCheckInTime.substring(STARTINDEX, ENDINDEXFOURLETTERS)); 
                break;
-            case THREELETTERSLONG: iCheckInHours = Character.getNumericValue(sCheckInTime.charAt(STARTINDEX));//turns first character of string to int
+            //turns first character of string to int, that will be the hour check in is at (ex: 850 is hour 8)   
+            case THREELETTERSLONG: iCheckInHours = Character.getNumericValue(sCheckInTime.charAt(STARTINDEX));
                break;
          
          }//End bracket for switch checking length of checkin
          
          switch (iCheckOutTimeLength) {//Start bracket for switch checking length of checkout, to avoid outofbounds index errors
            
-            case FOURLETTERSLONG: iCheckOutHours = Integer.parseInt(sCheckOutTime.substring(STARTINDEX, ENDINDEXFOURLETTERS));//turns first two characters into int
+            //turns first two characters of string to int, that will be the hour check in is at (ex: 1500 is hour 15) 
+            case FOURLETTERSLONG: iCheckOutHours = Integer.parseInt(sCheckOutTime.substring(STARTINDEX, ENDINDEXFOURLETTERS));
                break;
-            case THREELETTERSLONG: iCheckOutHours = Character.getNumericValue(sCheckOutTime.charAt(STARTINDEX));//turns first character into int
+            //turns first character of string to int, that will be the hour check in is at (ex: 850 is hour 8)    
+            case THREELETTERSLONG: iCheckOutHours = Character.getNumericValue(sCheckOutTime.charAt(STARTINDEX));
                break;
          
          }//End bracket for switch checking length of checkout
          
-         iHoursSpent = iCheckOutHours - iCheckInHours; //finding change in hours
-         iCheckInMinutes = Integer.parseInt(sCheckInTime.substring(iCheckInTimeLength - TENSPLACEOFFSET, iCheckInTimeLength)); //shortens string to last two chars, then converts to int
+         //finding change in hours
+         iHoursSpent = iCheckOutHours - iCheckInHours; 
+         //shortens string to last two chars, then converts to int. Amount of mins is always last two digits
+         iCheckInMinutes = Integer.parseInt(sCheckInTime.substring(iCheckInTimeLength - TENSPLACEOFFSET, iCheckInTimeLength)); 
          iCheckOutMinutes = Integer.parseInt(sCheckOutTime.substring(iCheckOutTimeLength - TENSPLACEOFFSET, iCheckOutTimeLength));
-         iMinutesSpent = iCheckOutMinutes - iCheckInMinutes;//finding change in minutes
+         //finding change in minutes
+         iMinutesSpent = iCheckOutMinutes - iCheckInMinutes;
          
          if (iMinutesSpent < MINMINUTES){//Start bracket for checking if minutes is negative
          
@@ -209,8 +238,10 @@ public class Wages{//Start bracket for Wages class
          
          }//End bracket for checking if minutes is negative
          
-         iHoursSpent += iMinutesSpent / SIXTYMINUTES; //Converts the minutes to hours
-         fTotalPay = iHoursSpent * fEmployeePay; //Converts hours to $
+         //Converts the minutes to hours
+         iHoursSpent += iMinutesSpent / SIXTYMINUTES; 
+         //Converts hours to $
+         fTotalPay = iHoursSpent * fEmployeePay; 
          //Final display of prints
          System.out.println("Employee Name:" + sEmployeeName);
          System.out.printf("Check in time: %d \n", iCheckInTime);
@@ -222,34 +253,42 @@ public class Wages{//Start bracket for Wages class
          
             try{//Start of try statement
          
-               System.out.println("Would you like to enter in another employee? (y/n)"); //Prompts user
-               cMoreEmployees = scanner.nextLine().charAt(0);//Stores next user character input
-               scanner = new Scanner(System.in); //resets scanner
+               System.out.println("Would you like to enter in another employee? (y/n)"); 
+               //resets scanner
+               scanner = new Scanner(System.in); 
+               //Stores next user character input
+               cMoreEmployees = scanner.nextLine().charAt(0);
+               
+               
                if (cMoreEmployees == 'y'){//Start of if statement checking if input is 'y'
          
                   for (iCounter = 0;iCounter < 3; iCounter++){ //for loop setting the past booleans to false
          
-                     bAllDone[iCounter] = false;//Sets all to false in order to restart
+                     //Sets all to false in order to restart
+                     bAllDone[iCounter] = false;
          
                   }//End bracket of for loop setting past booleans to false
-         
+                  
+                  //breaks out of the loop, since all booleans must be false to restart
                   break; 
          
                }//End bracket for if statement checking if input is 'y'
          
-               else if (cMoreEmployees != 'n'){//Start of if statement checking if input is anything but n or y
+               else if (cMoreEmployees != 'n'){//Start of if statement checking if input is anything but 'n' or 'y'
          
-                  throw new InputMismatchException();//goes to catch staetemnt
+                  //goes to catch statement
+                  throw new InputMismatchException();
          
-               }//End of if statement checking if input is anything but n or y
+               }//End of if statement checking if input is anything but 'n' or 'y'
          
-               else{//Start bracket for n input
+               else{//Start bracket for 'n' input
          
                   System.out.println("Thank you for using this program!");
+                  //Ends both while loops
                   bProgramDone = true;
                   bAllDone[MOREEMPLOYEESINDEX] = true;
         
-               }//End bracket for n input
+               }//End bracket for 'n' input
         
             }//End bracket for try statement
             
